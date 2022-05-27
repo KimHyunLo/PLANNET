cbd335aa0ac401e8bb98cc9449ae9e492f758d28
wasori <101244848+wasori@users.noreply.github.com>
2022-05-27 15:13:28 +0900

    Add files via upload

---
+++ option.js
@@ -0,0 +1,20 @@
+
+
+const popOpen = () =>{
+
+    var modalPop = $('.modal-wrap');
+    var modalBg = $('.modal-bg'); 
+
+    $(modalPop).show();
+    $(modalBg).show();
+
+};
+
+const popClose = () => {
+   var modalPop = $('.modal-wrap');
+   var modalBg = $('.modal-bg');
+
+   $(modalPop).hide();
+   $(modalBg).hide();
+
+};
