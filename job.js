5cf7311f5ef92e832c54190a43f54f88f3ce8ed3
wasori <bonjug33@naver.com>
2022-05-29 21:51:19 +0900

    직업

---
+++ job.js
@@ -0,0 +1,7 @@
+function move_left() {
+    document.querySelector(".image1_1").classList.add("go_left");
+}
+
+function move_right() {
+    document.querySelector(".image1_1").classList.add("go_right");
+}
